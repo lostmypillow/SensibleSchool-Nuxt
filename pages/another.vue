f066dbba83bfde1c5da8473e2558db1774024ad9
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-02-02 23:40:39 +0800

    02022340

---
+++ pages/another.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Logged In</h1>
+    </div>
+</template>
---
+++ pages/forgetpw.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>You forgot password, dafuq????</h1>
+    </div>
+</template>
